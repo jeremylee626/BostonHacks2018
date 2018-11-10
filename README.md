cbc0bf20f757ff6d8fae70337d6ead5c35df859d
Eric Iren Zhang <eric.iren.zhang@gmail.com>
2018-11-10 13:18:38 -0500

    setup initial flask framework, and updated README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BostonHacks18
 Boston Hacks 2018
+
+
+To run service, first run "python __init__.py"
+Then type in "localhost:5000" into web browser.
---
+++ buhacks_2018/__init__.py
@@ -0,0 +1,25 @@
+"""
+Praesidium project to analyses the IMP
+"""
+import os
+import json
+from datetime import datetime
+from flask import (
+    Flask, render_template, request, session, flash, url_for, redirect, jsonify
+)
+
+
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+@app.route('/api/v1.0/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
